cfa3fc017ec9ce2177367b7341fc03956575bd82
tanmayjain7856 <tanmayjain987@gmail.com>
2022-12-12 13:01:18 +0530

    Add javascript variables

---
+++ Programs/JavaScript/variables.js
@@ -0,0 +1,12 @@
+let year = 2022;
+console.log(year);
+
+let a = 4;
+let b = 6;
+let c = a + b;
+console.log(c);
+a = a + 1;
+console.log(a + b);
+console.log(c); // c is not changed. We've to change separately
+c = a + b;
+console.log(c); // c is changed now
