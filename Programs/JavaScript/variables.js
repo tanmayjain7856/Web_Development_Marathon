3aeb6fb5f8bc84080e9e2c599e7a80c53597ca76
tanmayjain7856 <tanmayjain987@gmail.com>
2022-12-12 13:19:25 +0530

    Add postfix and prefix incrementor

--- Programs/JavaScript/variables.js
+++ Programs/JavaScript/variables.js
@@ -10,3 +10,9 @@ console.log(a + b);
 console.log(c); // c is not changed. We've to change separately
 c = a + b;
 console.log(c); // c is changed now
+
+let score = 5;
+console.log(score++); //postfix increment returns value before incrementing by 1
+console.log(score);
+console.log(++score); //prefix increment returns value after increenting by 1
+console.log(score);
